2a6e6187b8d8b8aaa012c409b43003f8c59b5311
ahmedhamedaly <ahmedhamedaly19@gmail.com>
2023-10-06 20:52:06 +0200

    feat: landing page boilerplate

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,68 +1,28 @@
-import Link from "next/link";
 import type { NextPage } from "next";
-import { BugAntIcon, MagnifyingGlassIcon, SparklesIcon } from "@heroicons/react/24/outline";
 import { MetaHeader } from "~~/components/MetaHeader";
 
 const Home: NextPage = () => {
   return (
     <>
       <MetaHeader />
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center mb-8">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+      <div className="mx-auto max-w-7xl px-4">
+        <div className="h-screen flex flex-col items-center justify-center gap-6 px-16 -mt-44">
+          <h1 className="font-black text-6xl text-center">
+            Think of something to add here... <span className="text-primary">Color here </span>
           </h1>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/pages/index.tsx
-            </code>
+          <p className="font-medium text-center mx-28 opacity-90">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae eius provident voluptatum harum eaque
+            doloribus rem voluptatibus ducimus iste molestias laborum, fugit ullam quaerat magnam perspiciatis
+            architecto asperiores voluptatem mollitia!{" "}
           </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
-        </div>
+          <div className="flex gap-4 items-center">
+            <button className="btn btn-primary btn-sm normal-case h-12 w-40 text-sm" type="button">
+              Marketplace
+            </button>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contract
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <SparklesIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Experiment with{" "}
-                <Link href="/example-ui" passHref className="link">
-                  Example UI
-                </Link>{" "}
-                to build your own UI.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
+            <button className="btn btn-primary btn-outline btn-sm normal-case h-12 w-40 text-sm" type="button">
+              Watch video
+            </button>
           </div>
         </div>
       </div>
